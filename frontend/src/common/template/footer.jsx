670d6cd99ebd4bb8181635116251c18ff58073ea
Ronaldo Guastalli <guastalli31@gmail.com>
2018-06-06 16:29:41 -0300

    Componente Rodapé - Footer

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,17 @@
+/* 
+    - estrutura do rodape
+    - alterar app.js
+*/
+
+import React from 'react'
+
+export default props => (
+    <footer className = 'main-footer'>
+
+        <strong>
+            Copyright &copy; 2017
+            <a href='https://github.com/facebook/create-react-app' target='_blank'> React</a>.
+        </strong>
+
+    </footer>
+)
