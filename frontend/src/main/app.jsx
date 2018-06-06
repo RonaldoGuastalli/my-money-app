4531bc99e7dff230434d4445ad4163cf49b74631
Ronaldo Guastalli <guastalli31@gmail.com>
2018-06-06 14:16:40 -0300

    Componente Cabeçalho - Header

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,13 +1,15 @@
 /* 
     wrapper - do admin-lte
+    <Header /> -- componente header.jsx
 */
-
-import '../common/template/dependencies'
 
 import React from 'react'
 
+import '../common/template/dependencies'
+import Header from '../common/template/header'
+
 export default props => (
     <div className='wrapper'>
-        <h1>App</h1>
+        <Header />
     </div>
 )
