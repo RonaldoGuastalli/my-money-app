4ec9f6913c2f6e8b91dd037a5d88d5537157b4c5
Ronaldo Guastalli <guastalli31@gmail.com>
2018-06-06 12:02:03 -0300

    Componentes Index e App

---
+++ frontend/src/index.jsx
@@ -0,0 +1,11 @@
+/* 
+    reactDom -- para interagir com a pagina
+    <App /> -- vem do app.jxs
+    'app' -- é justamente o app que esta na div (<div id="app"></div>) em /frontend/public/index.html
+    gerou o bundle app.js
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './main/app'
+
+ReactDOM.render(<App />, document.getElementById('app'))
---
+++ frontend/src/main/app.jsx
@@ -0,0 +1,11 @@
+/* 
+    wrapper - do admin-lte
+*/
+
+import React from 'react'
+
+export default props => (
+    <div className='wrapper'>
+        <h1>App</h1>
+    </div>
+)
